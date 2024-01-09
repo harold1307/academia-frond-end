b6e8248aa4670f3f9e6772279d85a4dd402bece3
asgonzales <asgonzalesromani@gmail.com>
2024-01-08 21:25:57 -0300

    feat: Apply css in curso page

--- src/app/curso/page.tsx
+++ src/app/curso/page.tsx
@@ -4,8 +4,12 @@ import CursoTable from "./table";
 export default function CursoPage() {
 	return (
 		<>
-			<div className='mt-4'>
-				<AddCurso />
+			<div className='flex flex-col gap-4 justify-center align-center'>
+				<h1 className='w-100 text-center text-4xl'>Configuración de Cursos</h1>
+				<div>
+					<AddCurso />
+					{/* searchBar */}
+				</div>
 				<CursoTable />
 			</div>
 		</>
