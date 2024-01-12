cd968c2f2ab6d1aa526652a67a1509630612a3a5
asgonzales <asgonzalesromani@gmail.com>
2024-01-12 15:01:16 -0300

    fix: change style shadow to tailwind shadow

--- src/app/curso/page.tsx
+++ src/app/curso/page.tsx
@@ -2,7 +2,7 @@ import { fontPlay } from "../_components/ui/fonts";
 import LupaIcon from "../_components/ui/icons/lupa";
 import { Input } from "../_components/ui/input";
 import AddCurso from "./add-curso";
-import CursoTable from "./table";
+import CursoTable from "./cursosTable";
 
 export default function CursoPage() {
 	return (
@@ -11,10 +11,7 @@ export default function CursoPage() {
 				<div className='pl-6 pr-6 flex items-center justify-between'>
 					<AddCurso />
 					<div className='w-3/12 h-12 relative flex items-center'>
-						<Input className='h-100 rounded-xl'
-						 style={{
-							boxShadow: '0 0 25px rgba(67, 84, 234, .7)'
-						}} />
+						<Input className='h-100 rounded-xl shadow-primaryShadow' />
 						<div className='absolute right-3 h-5 flex justicy-center items-center' >
 							<LupaIcon/>
 						</div>
