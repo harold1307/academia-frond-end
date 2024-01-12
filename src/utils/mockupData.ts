c73e5951712d71e73858ce1fdc7f0c0c0b78ff99
asgonzales <asgonzalesromani@gmail.com>
2024-01-12 01:20:51 -0300

    feat: Add mockup data file

---
+++ src/utils/mockupData.ts
@@ -0,0 +1,28 @@
+import { CursoTableItem } from "@/app/curso/table/columns";
+
+export const isLoading = false
+
+export const MUCursos:CursoTableItem[] = [
+	{
+		id: '1',
+		alias: 'alias1',
+		certificado: 'certificado1',
+		estado: true,
+		nombre: 'nombre1'
+	},
+	{
+		id: '2',
+		alias: 'alias2',
+		certificado: 'certificado2',
+		estado: true,
+		nombre: 'nombre2'
+	},
+	{
+		id: '2',
+		alias: 'alias2',
+		certificado: 'certificado2',
+		estado: false,
+		nombre: 'nombre2'
+	},
+
+]
