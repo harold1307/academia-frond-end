9869f3b85b3b1bf710c8550f6c3701f4043d258d
asgonzales <asgonzalesromani@gmail.com>
2024-01-12 01:17:34 -0300

    feat: add custom boxShadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,6 +70,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      boxShadow: {
+        "primaryShadow": "0 0 20px rgba(67, 84, 234, .7)"
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
